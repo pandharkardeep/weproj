b3293629e2a786757b7c0f8cde4e1d9cf1c550f2
Deep P <pandharkardeep35@gmail.com>
2023-11-16 00:47:01 +0530

    WE PROJ

---
+++ backend/controllers/messageControllers.js
@@ -0,0 +1,64 @@
+const asyncHandler = require("express-async-handler");
+const Message = require("../models/messageModel");
+const User = require("../models/userModel");
+const Chat = require("../models/chatModel");
+
+
+const allMessages = asyncHandler(async (req, res) => {
+
+  try {
+    
+    //:chatId in routes //request params
+    const messages = await Message.find({ chat: req.params.chatId })
+      .populate("sender", "name email")
+      .populate("chat");
+
+    res.json(messages);
+    
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+
+
+const sendMessage = asyncHandler(async (req, res) => {
+
+  const { content, chatId } = req.body;
+
+  if (!content || !chatId) {
+    console.log("Invalid data passed into request");
+    return res.sendStatus(400);
+  }
+  //schema 
+  var newMessage = {
+    sender: req.user._id,
+    content: content,
+    chat: chatId,
+  };
+
+  try {
+    var message = await Message.create(newMessage);
+
+    //populating the instance
+    message = await message.populate("sender", "name");
+    message = await message.populate("chat");
+
+    //populating with the user in that chat field of our message doc instance
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name email",
+    });
+
+    await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
+
+    res.json(message);
+
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+module.exports = { allMessages, sendMessage };
